9db3119f3f9236bd71b6a698459e42f1f7845060
coldpigli <piyushdas98@gmail.com>
2021-03-01 15:46:41 +0530

    Added Splash Screen

--- app/src/main/java/com/example/logincbs/MainActivity.kt
+++ app/src/main/java/com/example/logincbs/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.logincbs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.lang.Thread.sleep
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_LoginCBS)
+        sleep(2000)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
