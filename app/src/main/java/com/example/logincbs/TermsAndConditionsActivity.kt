a7a53d67330af41a9073cf0c7da3f34a1b264fda
coldpigli <piyushdas98@gmail.com>
2021-03-02 11:48:20 +0530

    Added Login Activity, Enabled ViewBinding

--- app/src/main/java/com/example/logincbs/TermsAndConditionsActivity.kt
+++ app/src/main/java/com/example/logincbs/TermsAndConditionsActivity.kt
@@ -1,13 +1,28 @@
 package com.example.logincbs
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.logincbs.databinding.ActivityLoginBinding
+import com.example.logincbs.databinding.ActivityTermsAndCondititonsBinding
 import java.lang.Thread.sleep
 
 class TermsAndConditionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_LoginCBS)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_terms_and_condititons)
+        val binding = ActivityTermsAndCondititonsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonInTnc.setOnClickListener {
+            if(binding.checkbox.isChecked){
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+            }
+            else{
+                Toast.makeText(this, "Please Agree to the terms and Conditions", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
