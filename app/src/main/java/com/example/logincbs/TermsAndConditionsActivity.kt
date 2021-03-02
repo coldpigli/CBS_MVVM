27aa2ac4f98a428446dc48eee2789dc4f013f62a
coldpigli <piyushdas98@gmail.com>
2021-03-02 11:38:46 +0530

    Added terms and conditions XML

--- app/src/main/java/com/example/logincbs/TermsAndConditionsActivity.kt
+++ app/src/main/java/com/example/logincbs/TermsAndConditionsActivity.kt
@@ -4,11 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import java.lang.Thread.sleep
 
-class MainActivity : AppCompatActivity() {
+class TermsAndConditionsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_LoginCBS)
-        sleep(2000)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_terms_and_condititons)
     }
 }
